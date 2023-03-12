eae485fdb72cfdfab7de1003c57201351b76afac
WOON <youngwoon.kim@dream-academy.io>
2023-03-12 15:49:16 +0900

    practice01_makeStar:solve

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Pyramid {
     function run(uint size) pure public returns (string memory _out) {
-        _out = ""; // TODO
+        // _out = ""; // TODO
         /* Example (size = 5):
          *
          *     "*\n"
@@ -13,5 +13,19 @@ contract Pyramid {
          *     "*****\n"
          *
          */
+        _out = "";
+        for (uint i = 0; i < size;) {
+            for (uint j = 0; j <= i;) {
+                _out = string.concat(_out, "*");
+                unchecked {
+                    j++;
+                }
+            }
+            unchecked {
+                i++;
+            }
+            _out = string.concat(_out, "\n");
+        }
+
     }
 }
